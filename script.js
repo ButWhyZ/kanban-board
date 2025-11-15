be4ad2f2d5e1b3ee78e4d800139f67ea8db03bfb
Zairion Palmer <zairionpalmer@gmail.com>
2025-11-15 10:16:03 -0500

    Functional changes

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-
-function remove() {
-    const list = document.getElementById("glist");
-    list.removeChild(list.lastChild);
-} 
 function addItem() {
-    const item
-}   
+    const item = prompt("New item:");          
+    if (item) {                                
+        const li = document.createElement("li"); 
+        li.textContent = item;                 
+        document.getElementById("list").appendChild(li);
+    }
+}
+
+function removeItem() {
+    const list = document.getElementById("list");   
+    const last = list.lastElementChild;             
+    if (last) list.removeChild(last);  
+}
